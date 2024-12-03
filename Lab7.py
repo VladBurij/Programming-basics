cc5f10ea0381ed2d350acecad22cd7fa11eaec6b
VladBurij <burovvladislav4@gmail.com>
2024-12-03 12:55:01 +1000

    Доделал Lab7.py
    Доделал с 3 по 5 задание

--- Lab7.py
+++ Lab7.py
@@ -15,5 +15,26 @@ clients = [
     {"name": "Bob", "income": 120000},
     {"name": "Charlie", "income": 70000}
 ]
-clients_list = list(map(lambda x: x.update({"category": "Low"}), clients))
-print(clients_list)
+list(map(lambda x: x.update({"category": "Low"}) if x["income"] < 50000 else x.update({"category": "Medium"}) if x["income"] < 100000 else x.update({"category": "High"}), clients))
+print(clients)
+#Задание4
+flights = [
+    {"flight": "A1", "departure": 9, "arrival": 12},
+    {"flight": "B2", "departure": 14, "arrival": 18},
+    {"flight": "C3", "departure": 6, "arrival": 8}
+]
+flights_12 = list(filter(lambda x: x if x["arrival"] >= 12 else None, flights))
+print(flights_12)
+#Задание5
+messages = [
+    {"user": "Исследователь А", "message": "Отчёт готов. Ссылка: http://foundation.org"},
+    {"user": "Доктор Б", "message": "Документы можно найти здесь: https://classified.com"},
+    {"user": "Охранник В", "message": "Нет аномальной активности за смену."},
+    {"user": "Агент Г", "message": "Срочно изучите материалы по объекту 173 на http://statue-database.net"},
+    {"user": "Д-р Кляйн", "message": "Обновлённый протокол эксперимента доступен: https://safezone.scp"},
+    {"user": "Сотрудник Д", "message": "Просьба ознакомиться с https://docs.anomalies-secure.com перед сменой."},
+    {"user": "Старший учёный Л", "message": "Все записи переданы. Никаких аномалий на объекте 096."},
+    {"user": "Техник З", "message": "Проблема с сервером устранена. Подробнее: http://fix-report.internal"}
+]
+list(filter(lambda x: x.update({"message": "Ссылка: [ДАННЫЕ УДАЛЕНЫ]"}) if "http" in x["message"] else None, messages))
+print(messages)
